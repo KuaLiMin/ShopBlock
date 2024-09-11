83af625022367d088729990f9c1caf5c63f388ee
lczm <zemingchua@gmail.com>
2024-09-11 12:43:42 +0800

    feat: add a debug endpoint to show all existing users

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,12 +21,9 @@ from django.urls import include, path
 from backend.core import views
 from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"groups", views.GroupViewSet)
-
 urlpatterns = [
-    path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    # default django admin portal
     path("admin/", admin.site.urls),
+    # debugging purposes
+    path("debug/users/", views.DebugUserList.as_view()),
 ]
