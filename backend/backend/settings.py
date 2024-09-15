f374b26196f3bd30bf8e9da7059dd15ba8965889
lczm <zemingchua@gmail.com>
2024-09-15 19:10:43 +0800

    feat: set jwt duration to be rather long for easier dev

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,4 +146,9 @@ SPECTACULAR_SETTINGS = {
     # OTHER SETTINGS
     "SERVE_AUTHENTICATION": None,
     "COMPONENT_SPLIT_REQUEST": True,
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
 }
