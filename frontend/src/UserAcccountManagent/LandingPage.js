2a38970095424fd97b0c2443372831cfbceb79ac
theresatree <1a1.nghoeping@gmail.com>
2024-09-18 16:58:22 +0800

    changed css

--- frontend/src/UserAcccountManagent/LandingPage.js
+++ frontend/src/UserAcccountManagent/LandingPage.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './UserAccountManagment.css';
 
 function LandingPage() {
   return (
+<<<<<<< Updated upstream
     
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
+=======
+    <div className="container">
+>>>>>>> Stashed changes
       <a href="/user/login">
-        <button style={{ margin: '10px', padding: '10px 20px' }}>Login</button>
+        <button className="button">Login</button>
       </a>
       <a href="/user/signup">
-        <button style={{ margin: '10px', padding: '10px 20px' }}>Signup</button>
+        <button className="button">Signup</button>
       </a>
     </div>
   );
