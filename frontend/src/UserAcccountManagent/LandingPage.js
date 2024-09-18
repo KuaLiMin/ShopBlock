c400b49d07aa1951a2e643b177e80b64e873847b
theresatree <1a1.nghoeping@gmail.com>
2024-09-18 17:10:31 +0800

    changed css

--- frontend/src/UserAcccountManagent/LandingPage.js
+++ frontend/src/UserAcccountManagent/LandingPage.js
@@ -3,12 +3,8 @@ import './UserAccountManagment.css';
 
 function LandingPage() {
   return (
-<<<<<<< Updated upstream
     
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
-=======
-    <div className="container">
->>>>>>> Stashed changes
       <a href="/user/login">
         <button className="button">Login</button>
       </a>
