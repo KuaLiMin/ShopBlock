c43727b820c1b7b02537b3ca0bb842c40d68ee78
theresatree <1a1.nghoeping@gmail.com>
2024-09-29 19:08:57 +0800

    Create ListingDetail.jsx

---
+++ frontend/src/Pages/ListingDetail.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ListingDetail = () => {
+  return (
+    <View>
+      <Text>ListingDetail</Text>
+    </View>
+  )
+}
+
+export default ListingDetail
