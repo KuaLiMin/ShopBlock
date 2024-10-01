382e55a1573fbbcdc80a23ed85e6e97750b06dd1
theresatree <77893024+theresatree@users.noreply.github.com>
2024-10-01 16:18:53 +0800

    Create ListOfOffers.jsx

---
+++ frontend/src/Pages/ListOfOffers.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ListOfOffers = () => {
+  return (
+    <View>
+      <Text>ListOfOffers</Text>
+    </View>
+  )
+}
+
+export default ListOfOffers
