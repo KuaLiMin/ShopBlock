2bb3c80edad1c17d91275cbbf619dab5e6e70a97
JacksonSeowJX <35158303+JacksonSeowJX@users.noreply.github.com>
2024-10-09 17:54:20 +0800

    update

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
+
 export const SignUp = () => {
   // State for form fields
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const [usernameErrorMessage, setUsernameErrorMessage] = useState('');
+  const [emailErrorMessage, setEmailErrorMessage] = useState('');
   const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
   const [count, setCount] = useState(0);
   const [lock, setLock] = useState(false);
@@ -17,90 +18,78 @@ export const SignUp = () => {
 
   const navigate = useNavigate();
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   // Basic validation checks
-
-  //   if (!username || !password) {
-  //     setErrorMessage('All fields are required.');
-  //     return;
-  //   }
-  //   if (username !== "o") {
-  //     setUsernameErrorMessage('Username not found!');
-  //     return;
-  //   } else setUsernameErrorMessage('');
-
-  //   if (password !== "o" && count < 3) {
-  //       setPasswordErrorMessage('Incorrect Password! Please try again.');
-  //       setCount(count + 1);
-  //       return;
-  //   } else if (count === 3) {
-  //       setPasswordErrorMessage('Your account will be locked after two more attempts.');
-  //       setCount(count + 1);
-  //       return;
-  //   } else if (count === 4) {
-  //       setPasswordErrorMessage('Your account will be locked after one more attempt.');
-  //       setCount(count + 1);
-  //       return;
-  //   } else if (count === 5) {
-  //       setLock(true);
-  //       setLockMessage('Your account has been temporarily locked for 5 mins. Please try again later or ');
-  //       //setCount(0);
-  //       //setLock(false);
-  //       //setErrorMessage('');
-  //       return;
-  //   } else setPasswordErrorMessage('');
-
-  //   // If validation passes, clear the error message and submit the form
-
-  //   setErrorMessage('');
-  //   if (username && password) {
-  //     navigate('/');
-  //   }
-  // };
-
-
-  const handleSubmit = async (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault();
-    setError(null);  // Clear any previous error
+    // Basic validation checks
 
     const url = '/api/login/';
     const data = {
       email: email,
       password: password
     };
-    
+
     const headers = {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
       'X-CSRFTOKEN': 'wtZzU2TkoACioBvJdVSAaGNrtzjVDUKORZpRLZJqSajwuRqwKzzR9G2e2OokHVSP'
     };
 
+    if (!email || !password) {
+      setErrorMessage('All fields are required.');
+      return;
+    }
+    // if (email !== "o") {
+    //   setUsernameErrorMessage('Username not found!');
+    //   return;
+    // } else setUsernameErrorMessage('');
+
+    if (password !== "Qq@12345" && count < 3) {
+        setPasswordErrorMessage('Incorrect Password! Please try again.');
+        setCount(count + 1);
+        return;
+    } else if (count === 3) {
+        setPasswordErrorMessage('Your account will be locked after two more attempts.');
+        setCount(count + 1);
+        return;
+    } else if (count === 4) {
+        setPasswordErrorMessage('Your account will be locked after one more attempt.');
+        setCount(count + 1);
+        return;
+    } else if (count === 5) {
+        setLock(true);
+        setLockMessage('Your account has been temporarily locked for 5 mins. Please try again later or ');
+        //setCount(0);
+        //setLock(false);
+        //setErrorMessage('');
+        return;
+    } else setPasswordErrorMessage('');
+
+    // If validation passes, clear the error message and submit the form
+
     try {
       const response = await axios.post(url, data, { headers });
       console.log('Success:', response.data);
-      document.cookie = access=${response.data.access}; path=/;;
-      document.cookie = refresh=${response.data.refresh}; path=/;;
+      document.cookie = `access=${response.data.access}; path=/;`;
+      document.cookie = `refresh=${response.data.refresh}; path=/;`;
       window.location.href = '/browse';
     } catch (error) {
-      if (error.response && error.response.status === 401) {
-        setError('Invalid email or password');
-      } else {
-        setError('An error occurred. Please try again.');
-      }
       console.error('Error:', error);
+    }
+
+    setErrorMessage('');
+    if (email && password) {
+      navigate('/');
     }
   };
 
-  
   return (
     <div className='loginsignup'>
       <div className='loginsignup-container'>
         <h1>LOGIN</h1>
         <form onSubmit={handleSubmit}>
           <div className='loginsignup-fields'>
-            <input type='text' placeholder='Enter Username' value={username} onChange={(e) => setUsername(e.target.value)}/>
-            {usernameErrorMessage && <p style={{ color: 'red' }}>{usernameErrorMessage}</p>}
+            <input type='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+            {emailErrorMessage && <p style={{ color: 'red' }}>{emailErrorMessage}</p>}
             <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
             <div className='error-container'>
                 {passwordErrorMessage && !lock && <p style={{ color: 'red' }}>{passwordErrorMessage}</p>}
