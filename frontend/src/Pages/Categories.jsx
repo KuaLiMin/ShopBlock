70fb694088ff8a000e9d73d055c8ea8087186f40
Whitehathud <whitehathud@gmail.com>
2024-10-14 14:22:49 +0800

    Update Categories.jsx

--- frontend/src/Pages/Categories.jsx
+++ frontend/src/Pages/Categories.jsx
@@ -4,14 +4,12 @@ This is for the homepage (shop)
 import React from 'react'
 import Trending from '../components/Trending/Trending'
 import Hero from '../components/Hero/Hero'
-import Browse from '../components/Listing/Browse'
 
 export const Categories = () => {
   return (
     <div>
       <Hero/>
       <Trending.TrendingCounter />     
-
     </div>
   )
 }
