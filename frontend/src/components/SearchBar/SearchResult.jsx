53c2476268c893e71738a9d354d5980c1f230784
Whitehathud <whitehathud@gmail.com>
2024-10-29 12:12:00 +0800

    Update SearchResult.jsx

--- frontend/src/components/SearchBar/SearchResult.jsx
+++ frontend/src/components/SearchBar/SearchResult.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 import './SearchResult.css';
 import { Link } from 'react-router-dom';
 
-export const SearchResult = ({ result }) => {
+export const SearchResult = ({ results }) => {
+  if (!results) {
+    return (
+      <div className="search-result">
+        <p>No match found</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="search-result">
-      <Link
-        to={`/listing/${result.title}-${result.time ? result.time.replace(/-/g, '_') : 'unknown'}-${result.id}`}
-      >
-        {result.title}
-      </Link>
-    </div>
+    <>
+      {results.map((result) => (
+        <div className="search-result" key={result.id}>
+          <Link
+            to={`/listing/${result.title}-${result.time ? result.time.replace(/-/g, '_') : 'unknown'}-${result.id}`}
+          >
+            {result.title}
+          </Link>
+        </div>
+      ))}
+    </>
   );
 };
 
